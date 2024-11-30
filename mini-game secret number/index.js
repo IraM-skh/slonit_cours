28ed2e14ddf2da4f35986aac1c42b8a080c02448
Irina Makarova <Makarovaio1@yandex.ru>
2024-11-30 21:28:45 +0300

    Added binary number search
    -computer dialog is displayed in the console

--- mini-game secret number/index.js
+++ mini-game secret number/index.js
@@ -1,1 +1,36 @@
+const secretNumberGeneration = () => {
+  return Math.floor(1 + Math.random() * 100);
+};
+const hiddenNumber = secretNumberGeneration();
 
+const guessTest = (number) => {
+  if (hiddenNumber < number) {
+    return "Меньше.";
+  }
+  if (hiddenNumber > number) {
+    return "Больше.";
+  }
+  return "Угадал!";
+};
+
+const binarySearch = (startNumber, lastNumber) => {
+  const currentTestingNumber =
+    Math.floor((lastNumber - startNumber) / 2) + startNumber;
+
+  console.log(
+    `Компьютер 2: Поиск в диапазоне от ${startNumber} до ${lastNumber}. Пробую число ${currentTestingNumber}.`
+  );
+
+  const answer = guessTest(currentTestingNumber);
+  console.log(`Компьютер 1: ${answer}`);
+
+  if (answer === "Меньше.") {
+    binarySearch(startNumber, currentTestingNumber);
+  }
+  if (answer === "Больше.") {
+    binarySearch(currentTestingNumber, lastNumber);
+  }
+  return;
+};
+
+binarySearch(1, 100);
